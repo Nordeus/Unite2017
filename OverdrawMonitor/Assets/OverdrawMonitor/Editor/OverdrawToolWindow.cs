214e65259e70f450aaa1f9a296b341716b1b2865
 Kirill Belonogov <k_belonogov@wargaming.net>
2019-06-26 11:25:44 +0300

    Wip

--- OverdrawMonitor/Assets/OverdrawMonitor/Editor/OverdrawToolWindow.cs
+++ OverdrawMonitor/Assets/OverdrawMonitor/Editor/OverdrawToolWindow.cs
@@ -1,41 +1,54 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
-/// <summary> A simple tool to track the exact amount of overdraw in the game window. This tool
-/// only shows the result, <see cref="OverdrawMonitor"/> to check out how this is implemented. </summary>
+// A simple tool to track the exact amount of overdraw in the game window.
+// This tool only shows the result, see OverdrawMonitor to check out how this is implemented.
 public class OverdrawToolWindow : EditorWindow
 {
-	[MenuItem("Tools/Overdraw Tool")]
-	public static void ShowWindow()
-	{
-		var window = GetWindow<OverdrawToolWindow>();
-		window.Focus();
-	}
+    [MenuItem("Tools/Overdraw Tool")]
+    static void Show()
+    {
+        var window = GetWindow<OverdrawToolWindow>();
+        window.Focus();
+    }
 
-	public void OnGUI()
-	{
-		using (new GUILayout.HorizontalScope())
-		{
-			if (GUILayout.Button("Start"))
-			{
-				OverdrawMonitor.Instance.StartMeasurement();
-				OverdrawMonitor.Instance.ResetSampling();
-				OverdrawMonitor.Instance.ResetExtreemes();
-			}
+    [MenuItem("Tools/Overdraw Tool", true)]
+    static bool Validate()
+    {
+        return Application.isPlaying;
+    }
 
-			if (GUILayout.Button("End"))
-			{
-				OverdrawMonitor.Instance.Stop();
-			}
-		}
+    void OnGUI()
+    {
+        if (Validate())
+        {
+            using (new GUILayout.HorizontalScope())
+            {
+                if (GUILayout.Button("Start"))
+                {
+                    OverdrawMonitor.Instance.StartMeasurement();
+                    OverdrawMonitor.Instance.ResetSampling();
+                    OverdrawMonitor.Instance.ResetExtremes();
+                }
 
-		using (new GUILayout.HorizontalScope())
-		{
-			GUILayout.Label("Max\n" + OverdrawMonitor.Instance.MaxOverdraw.ToString("0.000"));
-			GUILayout.FlexibleSpace();
-			GUILayout.Label("Average\n" + OverdrawMonitor.Instance.AccumulatedAverageOverdraw.ToString("0.000"));
-		}
+                if (GUILayout.Button("End"))
+                {
+                    OverdrawMonitor.Instance.Stop();
+                }
+            }
 
-		Repaint();
-	}
+            using (new GUILayout.HorizontalScope())
+            {
+                GUILayout.Label("Max\n" + OverdrawMonitor.Instance.MaxOverdraw.ToString("0.000"));
+                GUILayout.FlexibleSpace();
+                GUILayout.Label("Average\n" + OverdrawMonitor.Instance.AccumulatedAverageOverdraw.ToString("0.000"));
+            }
+        }
+        else
+        {
+            GUILayout.Label("Available only in Play mode");
+        }
+
+        Repaint();
+    }
 }
--- OverdrawMonitor/Assets/OverdrawMonitor/OverdrawMonitor.cs
+++ OverdrawMonitor/Assets/OverdrawMonitor/OverdrawMonitor.cs
@@ -1,278 +1,236 @@
 ﻿using System;
 using UnityEngine;
 
-/// <summary> This is a singleton component that is responsible for measuring overdraw information
-/// on the main camera. You shouldn't add this compoenent manually, but use the Instance getter to
-/// access it.
-/// 
-/// The measurements process is done in two passes. First a new camera is created that will render
-/// the scene into a texture with high precission, the texture is called overdrawTexture. This texture
-/// contains the information how many times a pixel has been overdrawn. After this step a compute shader
-/// is used to add up all the pixels in the overdrawTexture and stores the information into this component.
-/// 
-/// We say this tool measures exactly the amount of overdraw, but it does so only in certain cases. In other
-/// cases the error margin is very small. This is because of the nature of the compute shaders. Compute
-/// shaders should operate in batches in order for them to be efficient. In our case the compute shader 
-/// batch that sums up the results of the first render pass has a size of 32x32. This means that if the
-/// pixel size of the camera is not divisible by 32, then the edge pixels that don't fit in range won't
-/// be processed. But since we usually have huge render targets (in comparison to 32x32 pixel blocks) and
-/// the error comes from the part of the image that is not important, this is acceptable. 
-/// </summary>
-[ExecuteInEditMode]
+// This is a singleton component that is responsible for measuring overdraw information
+// on the main camera. You shouldn't add this component manually, but use the Instance getter to
+// access it.
+//
+// The measurements process is done in two passes. First a new camera is created that will render
+// the scene into a texture with high precision, the texture is called overdrawTexture. This texture
+// contains the information how many times a pixel has been overdrawn. After this step a compute shader
+// is used to add up all the pixels in the overdrawTexture and stores the information into this component.
+//
+// We say this tool measures exactly the amount of overdraw, but it does so only in certain cases. In other
+// cases the error margin is very small. This is because of the nature of the compute shaders. Compute
+// shaders should operate in batches in order for them to be efficient. In our case the compute shader
+// batch that sums up the results of the first render pass has a size of 32x32. This means that if the
+// pixel size of the camera is not divisible by 32, then the edge pixels that don't fit in range won't
+// be processed. But since we usually have huge render targets (in comparison to 32x32 pixel blocks) and
+// the error comes from the part of the image that is not important, this is acceptable.
+
 public class OverdrawMonitor : MonoBehaviour
 {
-	private static OverdrawMonitor instance;
-	public static OverdrawMonitor Instance
-	{
-		get
-		{
-			if (instance == null)
-			{
-				instance = GameObject.FindObjectOfType<OverdrawMonitor>();
-				if (instance == null)
-				{
-					var go = new GameObject("OverdrawMonitor");
-					instance = go.AddComponent<OverdrawMonitor>();
-				}
-			}
+    const int GroupDimension = 32;
+    const int DataDimension = 128;
+    const int DataSize = DataDimension * DataDimension;
 
-			return instance;
-		}
-	}
+    public static OverdrawMonitor Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<OverdrawMonitor>();
+                if (instance == null)
+                {
+                    var go = new GameObject("OverdrawMonitor");
+                    DontDestroyOnLoad(go);
+                    go.hideFlags = HideFlags.DontSave;
+                    go.AddComponent<OverdrawMonitor>();
+                }
+            }
+            return instance;
+        }
+    }
 
-	private new Camera camera;
-	private RenderTexture overdrawTexture;
+    // Last measurement
+    // The number of shaded fragments in the last frame
+    public long TotalShadedFragments { get; private set; }
 
-	private ComputeShader computeShader;
+    // The overdraw ration in the last frame
+    public float OverdrawRatio { get; private set; }
 
-	private const int dataSize = 128 * 128;
-	private int[] inputData = new int[dataSize];
-	private int[] resultData = new int[dataSize];
-	private ComputeBuffer resultBuffer;
-	private Shader replacementShader;
+    // Sampled measurement
+    // Number of shaded fragments in the measured time span
+    public long IntervalShadedFragments { get; private set; }
 
-	// ========= Results ========
-	// Last measurement
-	/// <summary> The number of shaded fragments in the last frame. </summary>
-	public long TotalShadedFragments { get; private set; }
-	/// <summary> The overdraw ration in the last frame. </summary>
-	public float OverdrawRatio { get; private set; }
+    // The average number of shaded fragments in the measured time span
+    public float IntervalAverageShadedFragments { get; private set; }
 
-	// Sampled measurement
-	/// <summary> Number of shaded fragments in the measured time span. </summary>
-	public long IntervalShadedFragments { get; private set; }
-	/// <summary> The average number of shaded fragments in the measured time span. </summary>
-	public float IntervalAverageShadedFragments { get; private set; }
-	/// <summary> The average overdraw in the measured time span. </summary>
-	public float IntervalAverageOverdraw { get; private set; }
-	public float AccumulatedAverageOverdraw { get { return accumulatedIntervalOverdraw / intervalFrames; } }
+    // The average overdraw in the measured time span
+    public float IntervalAverageOverdraw { get; private set; }
+    public float AccumulatedAverageOverdraw => accumulatedIntervalOverdraw / intervalFrames;
 
-	// Extreems
-	/// <summary> The maximum overdraw measured. </summary>
-	public float MaxOverdraw { get; private set; }
+    // Extremes
+    // The maximum overdraw measured
+    public float MaxOverdraw { get; private set; }
 
-	private long accumulatedIntervalFragments;
-	private float accumulatedIntervalOverdraw;
-	private long intervalFrames;
+    static OverdrawMonitor instance;
+    Camera _camera;
+    RenderTexture overdrawTexture;
+    ComputeShader computeShader;
+    int[] inputData = new int[DataSize];
+    int[] resultData = new int[DataSize];
+    ComputeBuffer resultBuffer;
+    Shader replacementShader;
+    long accumulatedIntervalFragments;
+    float accumulatedIntervalOverdraw;
+    long intervalFrames;
+    float intervalTime = 0;
+    public float SampleTime = 1;
 
-	private float intervalTime = 0;
-	public float SampleTime = 1;
+    void Awake()
+    {
+        // Compute shader
+        computeShader = Resources.Load<ComputeShader>("OverdrawParallelReduction");
 
-	/// <summary> An empty method that can be used to initialize the singleton. </summary>
-	public void Touch() { }
+        // Replacement shader
+        replacementShader = Shader.Find("Debug/OverdrawInt");
+        Shader.SetGlobalFloat("OverdrawFragmentWeight", 1f / (GroupDimension * GroupDimension));
 
-	#region Measurement magic
+        // Camera
+        _camera = gameObject.AddComponent<Camera>();
 
-	public void Awake()
-	{
-#if UNITY_EDITOR
-		// Since this emulation always turns on by default if on mobile platform. With the emulation
-		// turned on the tool won't work.
-		UnityEditor.EditorApplication.ExecuteMenuItem("Edit/Graphics Emulation/No Emulation");
-		SubscribeToPlayStateChanged();
-#endif
-		
-		if (Application.isPlaying) DontDestroyOnLoad(gameObject);
-		gameObject.hideFlags = HideFlags.DontSave | HideFlags.HideInInspector;
+        for (int i = 0; i < inputData.Length; i++)
+            inputData[i] = 0;
 
-		// Prepare the camera that is going to render the scene with the initial overdraw data.
-		replacementShader = Shader.Find("Debug/OverdrawInt");
+        instance = this;
+    }
 
-		camera = GetComponent<Camera>();
-		if (camera == null) camera = gameObject.AddComponent<Camera>();
-		camera.CopyFrom(Camera.main);
-		camera.SetReplacementShader(replacementShader, null);
+    void LateUpdate()
+    {
+        Camera main = Camera.main;
 
-		RecreateTexture(Camera.main);
-		RecreateComputeBuffer();
+        _camera.CopyFrom(main);
+        _camera.clearFlags = CameraClearFlags.SolidColor;
+        _camera.backgroundColor = Color.black;
+        _camera.SetReplacementShader(replacementShader, null);
 
-		computeShader = Resources.Load<ComputeShader>("OverdrawParallelReduction");
+        RecreateTexture(main);
+        _camera.targetTexture = overdrawTexture;
 
-		for (int i = 0; i < inputData.Length; i++) inputData[i] = 0;
-	}
+        transform.position = main.transform.position;
+        transform.rotation = main.transform.rotation;
 
-#if UNITY_EDITOR
-	public void SubscribeToPlayStateChanged()
-	{
-		UnityEditor.EditorApplication.playmodeStateChanged -= OnPlayStateChanged;
-		UnityEditor.EditorApplication.playmodeStateChanged += OnPlayStateChanged;
-	}
+        intervalTime += Time.deltaTime;
+        if (intervalTime > SampleTime)
+        {
+            IntervalShadedFragments = accumulatedIntervalFragments;
+            IntervalAverageShadedFragments = (float)accumulatedIntervalFragments / intervalFrames;
+            IntervalAverageOverdraw = accumulatedIntervalOverdraw / intervalFrames;
 
-	private static void OnPlayStateChanged()
-	{
-		if (!UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode && UnityEditor.EditorApplication.isPlaying)
-		{
-			if (instance != null) instance.OnDisable();
-		}
-	}
-#endif
+            intervalTime -= SampleTime;
 
-	private bool disabled = true;
+            accumulatedIntervalFragments = 0;
+            accumulatedIntervalOverdraw = 0;
+            intervalFrames = 0;
+        }
+    }
 
-	public void OnEnable()
-	{
-		disabled = false;
-	}
+    // Checks if the overdraw texture should be updated. This needs to happen if the main camera configuration changes.
+    void RecreateTexture(Camera main)
+    {
+        if (overdrawTexture == null || main.pixelWidth != overdrawTexture.width || main.pixelHeight != overdrawTexture.height)
+        {
+            ReleaseTexture();
+            overdrawTexture = new RenderTexture(main.pixelWidth, main.pixelHeight, 24, RenderTextureFormat.RFloat);
+        }
+    }
 
-	public void OnDisable()
-	{
-		disabled = true;
-		OnDestroy();
-	}
+    void ReleaseTexture()
+    {
+        if (_camera != null)
+            _camera.targetTexture = null;
 
-	public void LateUpdate()
-	{
-		if (disabled) return;
+        if (overdrawTexture == null)
+            return;
 
-		Camera main = Camera.main;
-		camera.CopyFrom(main);
-		camera.clearFlags = CameraClearFlags.SolidColor;
-		camera.backgroundColor = Color.black;
-		camera.targetTexture = overdrawTexture;
-		camera.SetReplacementShader(replacementShader, null);
+        overdrawTexture.Release();
+        overdrawTexture = null;
+    }
 
-		transform.position = main.transform.position;
-		transform.rotation = main.transform.rotation;
+    void RecreateComputeBuffer()
+    {
+        if (resultBuffer == null)
+            resultBuffer = new ComputeBuffer(resultData.Length, 4);
+    }
 
-		RecreateTexture(main);
+    void OnDestroy()
+    {
+        ReleaseTexture();
 
-		intervalTime += Time.deltaTime;
-		if (intervalTime > SampleTime)
-		{
-			IntervalShadedFragments = accumulatedIntervalFragments;
-			IntervalAverageShadedFragments = (float)accumulatedIntervalFragments / intervalFrames;
-			IntervalAverageOverdraw = (float)accumulatedIntervalOverdraw / intervalFrames;
+        resultBuffer?.Release();
 
-			intervalTime -= SampleTime;
+        instance = null;
+    }
 
-			accumulatedIntervalFragments = 0;
-			accumulatedIntervalOverdraw = 0;
-			intervalFrames = 0;
-		}
-	}
+    void OnPostRender()
+    {
+        if (overdrawTexture == null)
+            return;
 
-	/// <summary> Checks if the overdraw texture should be updated. This needs to happen if the main camera
-	/// configuration changes. </summary>
-	private void RecreateTexture(Camera main)
-	{
-		if (overdrawTexture == null)
-		{
-			overdrawTexture = new RenderTexture(camera.pixelWidth, camera.pixelHeight, 24, RenderTextureFormat.RFloat);
-			overdrawTexture.enableRandomWrite = true;
-			camera.targetTexture = overdrawTexture;
-		}
+        int kernel = computeShader.FindKernel("CSMain");
 
-		if (main.pixelWidth != overdrawTexture.width || main.pixelHeight != overdrawTexture.height)
-		{
-			overdrawTexture.Release();
-			overdrawTexture.width = main.pixelWidth;
-			overdrawTexture.height = main.pixelHeight;
-		}
-	}
+        RecreateComputeBuffer();
 
-	private void RecreateComputeBuffer()
-	{
-		if (resultBuffer != null) return;
-		resultBuffer = new ComputeBuffer(resultData.Length, 4);
-	}
+        // Setting up the data
+        resultBuffer.SetData(inputData);
+        computeShader.SetInt("BufferSizeX", DataDimension);
+        computeShader.SetTexture(kernel, "Overdraw", overdrawTexture);
+        computeShader.SetBuffer(kernel, "Output", resultBuffer);
 
-	public void OnDestroy()
-	{
-		if (camera != null)
-		{
-			camera.targetTexture = null;
-		}
-		if (resultBuffer != null) resultBuffer.Release();
-	}
+        int xGroups = overdrawTexture.width / GroupDimension;
+        int yGroups = overdrawTexture.height / GroupDimension;
 
-	public void OnPostRender()
-	{
-		if (disabled) return;
+        // Summing up the fragments
+        computeShader.Dispatch(kernel, xGroups, yGroups, 1);
+        resultBuffer.GetData(resultData);
 
-		int kernel = computeShader.FindKernel("CSMain");
+        // Getting the results
+        TotalShadedFragments = 0;
+        for (int i = 0; i < resultData.Length; i++)
+            TotalShadedFragments += resultData[i];
 
-		RecreateComputeBuffer();
+        OverdrawRatio = (float)TotalShadedFragments / (xGroups * GroupDimension * yGroups * GroupDimension);
 
-		// Setting up the data
-		resultBuffer.SetData(inputData);
-		computeShader.SetTexture(kernel, "Overdraw", overdrawTexture);
-		computeShader.SetBuffer(kernel, "Output", resultBuffer);
+        accumulatedIntervalFragments += TotalShadedFragments;
+        accumulatedIntervalOverdraw += OverdrawRatio;
+        intervalFrames++;
 
-		int xGroups = (overdrawTexture.width / 32);
-		int yGroups = (overdrawTexture.height / 32);
+        if (OverdrawRatio > MaxOverdraw) MaxOverdraw = OverdrawRatio;
+    }
 
-		// Summing up the fragments
-		computeShader.Dispatch(kernel, xGroups, yGroups, 1);
-		resultBuffer.GetData(resultData);
+    //
+    // Measurement control methods
+    //
 
-		// Getting the results
-		TotalShadedFragments = 0;
-		for (int i = 0; i < resultData.Length; i++)
-		{
-			TotalShadedFragments += resultData[i];
-		}
+    public void StartMeasurement()
+    {
+        enabled = true;
+        GetComponent<Camera>().enabled = true;
+    }
 
-		OverdrawRatio = (float)TotalShadedFragments / (xGroups * 32 * yGroups * 32);
+    public void Stop()
+    {
+        enabled = false;
+        GetComponent<Camera>().enabled = false;
+    }
 
-		accumulatedIntervalFragments += TotalShadedFragments;
-		accumulatedIntervalOverdraw += OverdrawRatio;
-		intervalFrames++;
+    public void SetSampleTime(float time)
+    {
+        SampleTime = time;
+    }
 
-		if (OverdrawRatio > MaxOverdraw) MaxOverdraw = OverdrawRatio;
-	}
+    public void ResetSampling()
+    {
+        accumulatedIntervalOverdraw = 0;
+        accumulatedIntervalFragments = 0;
+        intervalTime = 0;
+        intervalFrames = 0;
+    }
 
-	#endregion
-	#region Measurement control methods
-
-	public void StartMeasurement()
-	{
-		enabled = true;
-		camera.enabled = true;
-	}
-	
-	public void Stop()
-	{
-		enabled = false;
-		camera.enabled = false;
-	}
-
-	public void SetSampleTime(float time)
-	{
-		SampleTime = time;
-	}
-
-	public void ResetSampling()
-	{
-		accumulatedIntervalOverdraw = 0;
-		accumulatedIntervalFragments = 0;
-		intervalTime = 0;
-		intervalFrames = 0;
-	}
-
-	public void ResetExtreemes()
-	{
-		MaxOverdraw = 0;
-	}
-
-	#endregion
+    public void ResetExtremes()
+    {
+        MaxOverdraw = 0;
+    }
 }
